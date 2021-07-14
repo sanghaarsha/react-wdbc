025722ca2073fc76d77704d6444251a3253a1815
sanghaarsha <sanghaarsha@gmail.com>
2021-07-14 09:16:08 +0545

    es6 destructure : object & arrays

---
+++ 3-es6-destructuring/src/index.js
@@ -0,0 +1,41 @@
+// CHALLENGE: uncomment the code below and see the car stats rendered
+import React from "react";
+import ReactDOM from "react-dom";
+import cars from "./practice";
+
+const [honda, tesla] = cars;
+
+const {
+  coloursByPopularity: [teslaTopColour]
+} = tesla;
+const {
+  speedStats: { topSpeed: teslaTopSpeed }
+} = tesla;
+
+const {
+  coloursByPopularity: [hondaTopColour]
+} = honda;
+const {
+  speedStats: { topSpeed: hondaTopSpeed }
+} = honda;
+
+ReactDOM.render(
+  <table>
+    <tr>
+      <th>Brand</th>
+      <th>Top Speed</th>
+      <th>Top Color</th>
+    </tr>
+    <tr>
+      <td>{tesla.model}</td>
+      <td>{teslaTopSpeed}</td>
+      <td>{teslaTopColour}</td>
+    </tr>
+    <tr>
+      <td>{honda.model}</td>
+      <td>{hondaTopSpeed}</td>
+      <td>{hondaTopColour}</td>
+    </tr>
+  </table>,
+  document.getElementById("root")
+);
