a53359de0b77a388a1059481f143d7c46fd27fe5
sanghaarsha <sanghaarsha@gmail.com>
2021-07-14 09:16:32 +0545

    useState hook

---
+++ 2-usestate-hook/src/components/App.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+
+function App() {
+  const [state, setCount] = useState(1);
+
+  function increase() {
+    setCount(state + 1);
+  }
+
+  function decrease() {
+    setCount(state - 1);
+  }
+
+  return (
+    <div className="container">
+      <h1>{state}</h1>
+      <button onClick={increase}>+</button>
+      <button onClick={decrease}>-</button>
+    </div>
+  );
+}
+
+export default App;
