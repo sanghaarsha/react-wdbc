28a9e889ada4cb00096d71d8d731535f36f0dcbf
sanghaarsha <sanghaarsha@gmail.com>
2021-07-14 09:19:36 +0545

    using es6 spread operator in react

---
+++ 9-es6-spread-operator/src/components/App.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+
+function App() {
+  const [contact, setContact] = useState({
+    fName: "",
+    lName: "",
+    email: ""
+  });
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+
+    setContact((prevState) => {
+      return {
+        ...prevState,
+        // to use a variable let say 'key' as a key
+        // we must use this syntax [key]:value_you_want
+        [name]: value
+      };
+    });
+  }
+
+  return (
+    <div className="container">
+      <h1>
+        Hello {contact.fName} {contact.lName}
+      </h1>
+      <p>{contact.email}</p>
+      <form>
+        <input
+          onChange={handleChange}
+          name="fName"
+          value={contact.fName}
+          placeholder="First Name"
+        />
+        <input
+          onChange={handleChange}
+          name="lName"
+          value={contact.lName}
+          placeholder="Last Name"
+        />
+        <input
+          onChange={handleChange}
+          name="email"
+          value={contact.email}
+          placeholder="Email"
+        />
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
